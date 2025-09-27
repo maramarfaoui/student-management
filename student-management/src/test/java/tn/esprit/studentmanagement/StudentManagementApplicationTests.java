a6481692ce02698400739ce2155c67a26d18e829
Maram Arfaoui <92606780+maramarfaoui@users.noreply.github.com>
2025-09-27 12:02:01 +0100

    Update StudentManagementApplicationTests.java

--- student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,8 +2,8 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
+@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
